1c50d933f365efdc04463a65e1d6ada51d48432d
Muhammad Aladdin <m.aladdin@breadfast.com>
2021-02-16 18:57:29 +0200

    jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
---
+++ src/__tests__/login.test.ts
@@ -0,0 +1,5 @@
+describe("login cases", () => {
+  it("should allow user to login if data is valid", async () => {
+    expect(true).toBe(true);
+  });
+});
